d452fafc7df2cdb2ab20536ee637aef8f7a4c269
John Ostlund <robotron2084@protonmail.com>
2019-11-07 14:30:19 -0600

    Initial Commit

---
+++ MarsViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarsViewer
+//
+//  Created by John Ostlund on 11/7/19.
+//  Copyright © 2019 John Ostlund. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
